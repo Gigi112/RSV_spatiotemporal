b49e4285e9cff3381f3bc17c1e3b627c39085f83
Gigi112 <709141475@qq.com>
2021-02-05 10:57:22 -0500

    Add files via upload
    comments

--- first_stage.R
+++ first_stage.R
@@ -25,57 +25,64 @@ for(j in 1:n.date){
 y[i,j]  ~ dpois(lambda[i,j])
 
 log(lambda[i,j])<- logpop[i]+ beta0[i] + amp[i]*cos(2*pi*j/12 - phase[i]) + phi[i,j]
+# harmonic Poisson regression model
 
 phi[i,j] ~ dnorm (0,tau3[i])
 }
 
-phase[i] <- (2*pi*(exp(trans[i]))) / (1+exp(trans[i]))
-beta0[i] ~ dnorm(0, 0.001)
+phase[i] <- (2*pi*(exp(trans[i]))) / (1+exp(trans[i])) # support estimates in real line
+beta0[i] ~ dnorm(0, 0.001) # priors
 amp[i]<-exp(beta1[i])
 beta1[i] ~ dnorm(0,0.001) 
 trans[i] ~ dnorm(0,0.001)
-tau3[i] ~ dgamma(1.00, 0.01)} 
+tau3[i] ~ dgamma(0.01, 0.01)} 
 }"
 
 dataset <- list('y' = y,"logpop"=logpop, "n.zip"=zip,"n.date"=date,pi=pi)
 
 model<-jags.model(textConnection(model_string),
                    data=dataset,
-                   n.chains=2)
+                   n.chains=1)
 update(model, 
-       n.iter=n)
+       n.iter=n) # burn-in 
 
 modelresult<-coda.samples(model, variable.names=c("trans"),
                       thin=10,
-                      n.iter=m)
+                      n.iter=m) # posterior samples
 result <- as.data.frame(modelresult[[1]])
 first.result<<-result
 }
 
 #run model for CT
 firststage(y=CTwide,logpop=ctunder5$log,zip=nrow(CTwide),date=ncol(CTwide),n=10000,m=50000)
+firstct <- first.result
 cttransmean<- colMeans(first.result)  # posterior mean of phase estimates
 var.ct <- c()
 for (i in 1:275) {
   var.ct[i] <- var(first.result[,i])} # calculate variations of phase estimates
+saveRDS(firstct,"firstct.rds")  # save posterior estimates for the second stage model
 saveRDS(var.ct,"varct.rds")           # save variations for the second stage
 saveRDS(cttransmean,"transct.rds")   # save phase estimates for the second stage
 
 #run model for NY
 firststage(y=NYwide,logpop=nyunder5$log,zip=nrow(NYwide),date=ncol(NYwide),n=10000,m=50000)
+firstny <- first.result
 nytransmean<- colMeans(first.result)  # posterior mean of phase estimates
 var.ny <- c()
 for (i in 1:1745) {
   var.ny[i] <- var(first.result[,i])} # calculate variations of phase estimates
+saveRDS(firstny,"firstny.rds")  # save posterior estimates for the second stage model
 saveRDS(var.ny,"varny.rds")           # save variations for the second stage
 saveRDS(first.result,"transny.rds")   # save phase estimates for the second stage
 
 #run model for NJ
 firststage(y=NJwide,logpop=njunder5$log,zip=nrow(NJwide),date=ncol(NJwide),n=10000,m=50000)
+firstnj <- first.result
 njtransmean<- colMeans(first.result)  # posterior mean of phase estimates
 var.nj <- c()
 for (i in 1:592) {
   var.nj[i] <- var(first.result[,i])} # calculate variations of phase estimates
+saveRDS(firstnj,"firstnj.rds")  # save posterior estimates for the second stage model
 saveRDS(var.nj,"varnj.rds")           # save variations for the second stage
 saveRDS(first.result,"transnj.rds")   # save phase estimates for the second stage
 
